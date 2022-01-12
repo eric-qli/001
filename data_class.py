44adcdc623bc1a4f964ed59c27a9d646528fc6e4
eric-qli <92590396+eric-qli@users.noreply.github.com>
2022-01-12 00:38:15 -0800

    Add files via upload

---
+++ data_class.py
@@ -0,0 +1,94 @@
+"""
+CSC110 FINAL PROJECT - The connection between COVID-19 and Crime
+Dataclasses
+Yinuo Zhao
+
+This files contains the two data classes needed for this program.
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class CrimeData:
+    """
+    A custom data type that represents data for crime cases of a specific area, type, and time(year and  month)
+
+    Instance Attributes:
+        - year: the year of this line of data
+        - month: the month of this line of data
+        - area: the area of which this line of data is recorded in
+        - type: type of the crime
+        - number: number of cases
+
+    Representation Invariants:
+        - self.year > 0
+        - self.month >0
+        - len(self.area) >= 0
+        - len(self.type) >= 0
+        - self.number >= 0
+
+    >>> crimedata = CrimeData(year=2019, month=3, area='', type='', number=0)
+    >>> crimedata
+    CrimeData(year=2019, month=3, area='', type='', number=0)
+    """
+    year: int
+    month: int
+    area: str
+    type: str
+    number: int
+
+
+@dataclass
+class CovidData:
+    """
+    A custom data type that represents data for COVID cases of a specific province, and time(year month day)
+
+    Instance Attributes:
+        - year: The year of this data
+        - month: The month of this data
+        - day: The day of this data
+        - province: Name of the province
+        - total_case: Number of total cases (confirmed + probable),Number of confirmed cases in
+                    each P/T (Canada= sum of all P/Ts)
+        - daily_case: Number of new cases since last update,Number of total counts from last update
+                    subtracted from total counts of current update
+
+    Representation Invariants:
+        - self.year > 0
+        - self.month > 0
+        - self.day > 0
+        - len(self.province) > 0
+        - self.total_case > 0
+        - self.daily_case > 0
+        - self.daily_case <= self.total_case
+
+    >>> covid_data = CovidData(year=2019, month=3, day=25, province='Ontario', total_case=25, daily_case=10)
+    >>> covid_data
+    CovidData(year=2019, month=3, day=25, province='Ontario', total_case=25, daily_case=10)
+
+    """
+    year: int
+    month: int
+    day: int
+    province: str
+    total_case: int
+    daily_case: int
+
+
+if __name__ == '__main__':
+
+    import python_ta
+
+    python_ta.check_all(config={
+        'extra-imports': ['python_ta.contracts'],
+        'allowed-io': [],
+        'max-line-length': 120,
+        'disable': []
+    })
+
+    import python_ta.contracts
+    python_ta.contracts.check_all_contracts()
+
+    import doctest
+    doctest.testmod()
---
+++ load_data.py
@@ -0,0 +1,69 @@
+"""
+CSC110 FINAL PROJECT - The connection between COVID-19 and Crime
+Load data
+Yinuo Zhao
+
+This files contains the two function need to load the datasets to the objects we created.
+"""
+
+import csv
+from data_class import CrimeData, CovidData
+
+
+def load_crime_data(filename: str) -> list[CrimeData]:
+    """
+    This function loads the data in the file to a list of CrimeData object
+    """
+    data_so_far = []
+    with open(filename) as f:
+        reader = csv.reader(f, delimiter=',')
+        next(reader)  # SKIP THE HEADER
+        for row in reader:
+            if row[11] == '':
+                data_so_far.append(CrimeData(year=int(row[0].split('-')[0]),
+                                             month=int(row[0].split('-')[1]),
+                                             area=row[1],
+                                             type=row[3],
+                                             number=0))
+            else:
+                data_so_far.append(CrimeData(year=int(row[0].split('-')[0]),
+                                             month=int(row[0].split('-')[1]),
+                                             area=row[1],
+                                             type=row[3],
+                                             number=int(row[11])))
+    return data_so_far
+
+
+def load_covid_data(filename: str) -> list[CovidData]:
+    """
+    This function loads the data in the file to a list of CovidData object
+    """
+    data_so_far = []
+    with open(filename) as f:
+        reader = csv.reader(f, delimiter=',')
+        next(reader)  # SKIP THE HEADER
+        for row in reader:
+            data_so_far.append(CovidData(year=int(row[3].split('-')[0]),
+                                         month=int(row[3].split('-')[1]),
+                                         day=int(row[3].split('-')[2]),
+                                         province=row[1],
+                                         total_case=int(row[8]),
+                                         daily_case=int(row[15])))
+    return data_so_far
+
+
+if __name__ == '__main__':
+    import python_ta
+
+    python_ta.check_all(config={
+        'extra-imports': ['python_ta.contracts', 'csv', 'data_class'],
+        'allowed-io': ['load_crime_data', 'load_covid_data'],
+        'max-line-length': 120,
+        'disable': []
+    })
+
+    import python_ta.contracts
+    python_ta.contracts.check_all_contracts()
+
+    import doctest
+    doctest.testmod()
